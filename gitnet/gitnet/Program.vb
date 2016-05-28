0c2b99364f35f7c027044ac8943d4c3367cf43bc
xieguigang <xie.guigang@gmail.com>
2016-05-28 22:42:14 +0800

    add test

---
+++ gitnet/gitnet/Program.vb
@@ -0,0 +1,9 @@
+﻿Imports Microsoft.VisualBasic.Webservices.Github.API.Search
+
+Module Program
+
+    Sub Main()
+        Dim r = Microsoft.VisualBasic.Webservices.Github.API.Search.Users(New UsersQuery With {.term = "xieguigang"})
+        Dim user = Microsoft.VisualBasic.Webservices.Github.API.GetUser("xieguigang")
+    End Sub
+End Module
