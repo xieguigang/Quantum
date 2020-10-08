af5b6555a0fa859661cc64e3a6c07a2ba2117014
xieguigang <xie.guigang@gcmodeller.org>
2020-10-08 13:01:16 +0800

    Update ListenServer.vb

--- services/ListenServer.vb
+++ services/ListenServer.vb
@@ -1,18 +1,56 @@
-﻿Imports Microsoft.VisualBasic.Net
+﻿Imports Microsoft.VisualBasic.ComponentModel
+Imports Microsoft.VisualBasic.Net
 Imports Microsoft.VisualBasic.Net.Protocols.Reflection
 Imports Microsoft.VisualBasic.Net.Tcp
 Imports Microsoft.VisualBasic.Parallel
 Imports Proxy.Protocol
 
 <Protocol(GetType(Protocols))>
-Public Class ListenServer
+Public Class ListenServer : Implements ITaskDriver, IDisposable
 
     Dim socket As TcpServicesSocket
     Dim protocol As New ProtocolHandler(Me)
 
-    <Protocol(Protocols.requestWeb)>
-    Private Function requestWeb(request As RequestStream, remoteDevcie As IPEndPoint) As RequestStream
+    Private disposedValue As Boolean
 
+    Sub New(port As Integer)
+        socket = New TcpServicesSocket(port) With {
+            .ResponseHandler = AddressOf protocol.HandleRequest
+        }
+    End Sub
+
+    Public Function Run() As Integer Implements ITaskDriver.Run
+        Return socket.Run
     End Function
 
+    <Protocol(Protocols.requestWeb)>
+    Private Function requestWeb(request As RequestStream, remoteDevcie As IPEndPoint) As RequestStream
+        Dim pkg = requestPackage(Of requestWeb).CreateObject(request.ChunkBuffer)
+    End Function
+
+    Protected Overridable Sub Dispose(disposing As Boolean)
+        If Not disposedValue Then
+            If disposing Then
+                ' TODO: dispose managed state (managed objects)
+                Call socket.Dispose()
+            End If
+
+            ' TODO: free unmanaged resources (unmanaged objects) and override finalizer
+            ' TODO: set large fields to null
+            disposedValue = True
+        End If
+    End Sub
+
+    ' ' TODO: override finalizer only if 'Dispose(disposing As Boolean)' has code to free unmanaged resources
+    ' Protected Overrides Sub Finalize()
+    '     ' Do not change this code. Put cleanup code in 'Dispose(disposing As Boolean)' method
+    '     Dispose(disposing:=False)
+    '     MyBase.Finalize()
+    ' End Sub
+
+    Public Sub Dispose() Implements IDisposable.Dispose
+        ' Do not change this code. Put cleanup code in 'Dispose(disposing As Boolean)' method
+        Dispose(disposing:=True)
+        GC.SuppressFinalize(Me)
+    End Sub
 End Class
