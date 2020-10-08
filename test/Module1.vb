a310f0e0079b4da8401a5871dfe51dd7f33e1c27
xieguigang <xie.guigang@gcmodeller.org>
2020-10-08 12:48:27 +0800

    config test project

--- test/Module1.vb
+++ test/Module1.vb
@@ -1,7 +1,19 @@
-﻿Module Module1
+﻿Imports Proxy.Protocol
+
+Module Module1
 
     Sub Main()
+        Dim obj As New requestWeb With {
+            .headers = New Dictionary(Of String, String) From {{"a", "b"}},
+            .method = "GET",
+            .url = "http://127.0.0.1"
+        }
 
+        Dim tmp As Byte() = requestPackage(Of requestWeb).Create(obj).Serialize
+
+        Dim obj2 = requestPackage(Of requestWeb).CreateObject(tmp)
+
+        Pause()
     End Sub
 
 End Module
