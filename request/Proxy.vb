5d137134afbaa7fab88dd9968a11e6e7596fbf06
xieguigang <xie.guigang@gcmodeller.org>
2020-10-08 15:11:33 +0800

    proxy object

---
+++ request/Proxy.vb
@@ -0,0 +1,18 @@
+﻿Imports Proxy.Protocol
+
+Public Class Proxy
+
+    ReadOnly proxy As String
+
+    Sub New(proxy As String)
+        Me.proxy = proxy
+    End Sub
+
+    Public Function GetText(url As String) As String
+        Return Handlers.GetHttpText(url, proxy)
+    End Function
+
+    Public Overrides Function ToString() As String
+        Return proxy
+    End Function
+End Class
