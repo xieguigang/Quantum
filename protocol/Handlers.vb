fe0f1dc07ee4230585ad5c1c9e051ee0b7cb2773
xieguigang <xie.guigang@gcmodeller.org>
2020-10-08 15:08:07 +0800

    get http text

--- protocol/Handlers.vb
+++ protocol/Handlers.vb
@@ -1,3 +1,26 @@
-﻿Module Module1
+﻿Imports System.Text
+Imports Microsoft.VisualBasic.Net
+Imports Microsoft.VisualBasic.Net.Tcp
+Imports Microsoft.VisualBasic.Parallel
+Imports Microsoft.VisualBasic.Text
 
+Public Module Handlers
+
+    ReadOnly textEncoder As Encoding = Encodings.UTF8.CodePage
+
+    Public Function GetHttpText(url As String, proxy As String, Optional headers As Dictionary(Of String, String) = Nothing) As String
+        Dim obj As New requestWeb With {
+            .headers = If(headers, New Dictionary(Of String, String)),
+            .method = "GET",
+            .url = url
+        }
+
+        Dim message = requestPackage(Of requestWeb).Create(obj)
+        Dim request As New RequestStream(Globals.protocol, Protocols.requestWeb, message.Serialize)
+        Dim result As RequestStream = New TcpRequest(New IPEndPoint(proxy)).SendMessage(request)
+        Dim tmp As Byte() = Globals.DecryptData(message.random, result.ChunkBuffer)
+        Dim text As String = textEncoder.GetString(tmp)
+
+        Return text
+    End Function
 End Module
--- test/Module1.vb
+++ test/Module1.vb
@@ -1,29 +1,11 @@
 ﻿Imports System.Threading
-Imports Microsoft.VisualBasic.Net
-Imports Microsoft.VisualBasic.Net.Tcp
-Imports Microsoft.VisualBasic.Parallel
-Imports Microsoft.VisualBasic.Text
 Imports Proxy.Protocol
 
 Module Module1
 
     Sub Main()
-        Dim obj As New requestWeb With {
-            .headers = New Dictionary(Of String, String) From {{"a", "b"}},
-            .method = "GET",
-            .url = "http://baidu.com"
-        }
-
-        Dim message = requestPackage(Of requestWeb).Create(obj)
-        Dim tmp As Byte() = message.Serialize
-
         Call Thread.Sleep(1000)
-
-        Dim request As New RequestStream(Globals.protocol, Protocols.requestWeb, tmp)
-        '   Dim obj2 = requestPackage(Of requestWeb).CreateObject(tmp)
-        Dim result As RequestStream = New TcpRequest(New IPEndPoint("127.0.0.1:232")).SendMessage(request)
-
-        Dim text As String = Encodings.UTF8.CodePage.GetString(Globals.DecryptData(message.random, result.ChunkBuffer))
+        Call Console.WriteLine(Handlers.GetHttpText("https://baidu.com", "127.0.0.1:232"))
 
         Pause()
     End Sub
