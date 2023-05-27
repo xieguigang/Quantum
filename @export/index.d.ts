5cdc6272cd1bc2aea430eadd225acc8a2297cceb
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2023-05-28 07:35:20 +0800

    build package

---
+++ @export/index.d.ts
@@ -0,0 +1,11 @@
+﻿// export R# source type define for javascript/typescript language
+//
+// package_source=Quantum
+
+declare namespace Quantum {
+   module _ {
+      /**
+      */
+      function onLoad(): object;
+   }
+}
